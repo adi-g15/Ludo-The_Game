aa63e23c707263fbf83f58a7ac5258433f4fc767
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-31 20:21:36 +0530

    Fix deepcode warning

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Dice from './components/Dice';
 import Table from './components/Table';
 import Board from './components/Board';
-import { rollDice, moveGoti } from "./services/ludo"
+import { rollDiceOnce, moveGoti } from "./services/ludo"
 
 function App() {
 	const turns = ['Green', 'Yellow', 'Blue', 'Red'];
@@ -36,159 +36,144 @@ function App() {
 		setInfo('');
 		const coords = current_place.split('-');											// Coordinates of current place taken from 'Goti'
 
-		try{
-			const {isPossible, finalCoords} = moveGoti(
-				['G', 'Y', 'B', 'R'][turn],	//colour
-				[parseInt(coords[0]), parseInt(coords[1])],	//coords
-				die	// distance_to_move
-			);
+		const {isPossible, finalCoords} = moveGoti(
+			['G', 'Y', 'B', 'R'][turn],	//colour
+			[parseInt(coords[0]), parseInt(coords[1])],	//coords
+			die	// distance_to_move
+		);
 
-			if (isPossible[0]) {
-				const updated_place = finalCoords[0][0] + '-' + finalCoords[0][1];	// updated coordinates from response
-				let newboard = { ...board };													// Copy of boards & positions to make changes
-				let position = [...positions];												// before setting the state
-				newboard[current_place].splice(newboard[current_place].indexOf(turn), 1);	// Goti removed from current place
-				if ({ '7-6': 0, '6-7': 1, '7-8': 2, '8-7': 3 }[updated_place] === turn) {	// Check if updated position is the final position
-					position[turn][position[turn].indexOf(current_place)] = true;			// of current turn.If yes, position of current goti
-					if (positions[turn].reduce((total, curr) => total + (curr === true), 0) === 4) {	// is set 'true', meaning reached end.
-						alert(turns[turn] + ' won the Game! Starting a new game...');		// If the count of 'true' in position of current
-						window.location.reload();											// turn = 4, means current player won the game.
-					}
+		if (isPossible[0]) {
+			const updated_place = finalCoords[0][0] + '-' + finalCoords[0][1];	// updated coordinates from response
+			let newboard = { ...board };													// Copy of boards & positions to make changes
+			let position = [...positions];												// before setting the state
+			newboard[current_place].splice(newboard[current_place].indexOf(turn), 1);	// Goti removed from current place
+			if ({ '7-6': 0, '6-7': 1, '7-8': 2, '8-7': 3 }[updated_place] === turn) {	// Check if updated position is the final position
+				position[turn][position[turn].indexOf(current_place)] = true;			// of current turn.If yes, position of current goti
+				if (positions[turn].reduce((total, curr) => total + (curr === true), 0) === 4) {	// is set 'true', meaning reached end.
+					alert(turns[turn] + ' won the Game! Starting a new game...');		// If the count of 'true' in position of current
+					window.location.reload();											// turn = 4, means current player won the game.
 				}
-				else {
-					position[turn][position[turn].indexOf(current_place)] = updated_place;	// Update new positions to updatd position of goti
-					if (!['8-2', '2-6', '6-12', '12-8'].includes(updated_place)) {			// If updated position is not a safe block, check
-						newboard[updated_place] = newboard[updated_place].reduce((result, current) => {	// if the updated place has any gotis.
-							if (current === turn) {											// If there are gotis of same color, keep adding
-								result.push(current);										// them to an array to finally update the whole
-							}																// block with all those gotis.
-							else {
-								for (let i = 0; i < 4; ++i) {										// If there are gotis of another color, kill them
-									if (position[current][i] && position[current][i] === updated_place) {	// by not adding them to result
-										position[current][i] = false;						// array and setting their position as false(home)
-									}
-								}
-							}
-							return result;													// return result array to update board block
-						}, []);
-					}
-					newboard[updated_place].push(turn);										// Finally add current moved goti to the board
-				}
-				setPositions(position);														// Setstate of positions and board arrays
-				setBoard(newboard);
-				if (die < 6)
-					setTurn(turn => (turn + 1) % 4);												// If die value < 6, pass turn to next player
-				setChoice(true);
 			}
 			else {
-				setInfo('Invalid move, select another Goti');								// If server responds 'false' for move, give error
+				position[turn][position[turn].indexOf(current_place)] = updated_place;	// Update new positions to updatd position of goti
+				if (!['8-2', '2-6', '6-12', '12-8'].includes(updated_place)) {			// If updated position is not a safe block, check
+					newboard[updated_place] = newboard[updated_place].reduce((result, current) => {	// if the updated place has any gotis.
+						if (current === turn) {											// If there are gotis of same color, keep adding
+							result.push(current);										// them to an array to finally update the whole
+						}																// block with all those gotis.
+						else {
+							for (let i = 0; i < 4; ++i) {										// If there are gotis of another color, kill them
+								if (position[current][i] && position[current][i] === updated_place) {	// by not adding them to result
+									position[current][i] = false;						// array and setting their position as false(home)
+								}
+							}
+						}
+						return result;													// return result array to update board block
+					}, []);
+				}
+				newboard[updated_place].push(turn);										// Finally add current moved goti to the board
 			}
-		} catch(err) {
-			alert(typeof err === "string" ? err : "Move nahi kiya ja sakta... Try again...");
-			console.error(err);
+			setPositions(position);														// Setstate of positions and board arrays
+			setBoard(newboard);
+			if (die < 6)
+				setTurn(turn => (turn + 1) % 4);												// If die value < 6, pass turn to next player
+			setChoice(true);
+		}
+		else {
+			setInfo('Invalid move, select another Goti');								// If server responds 'false' for move, give error
 		}
 
 	};
-	async function checkPossible(roll) {
-	let flag = true;																		// Flag checks if avalable gotis are at end part
-	let locked = false;																		// Locked checks if there are any locked gotis
+	function checkPossible(roll) {
+		let flag = true;																// Flag checks if avalable gotis are at end part
+		let locked = false;																// Locked checks if there are any locked gotis
 		let coords = [];
-		// eslint-disable-next-line no-unused-vars
-		return new Promise((resolve, reject) => {
-			positions[turn].forEach(value => {
-				if (typeof (value) === 'string') {
-					const coord = value.split('-');											// If gotis are not at the last(colored) part
-					coords.push([parseInt(coord[0]), parseInt(coord[1])]);					// of the board, any move is possible and hence
-					if (coord[turn % 2] !== '7' || (coord[turn % 2] === '7' && ['0', '14'].includes(coord[(turn % 2) ^ 1]))) {	// set flag=false
-						flag = false;
-					}
-				}
-				else if (value === false) {													// If any gotis are locked, set locked=true
-					locked = true;
-				}
-			});
-			if (flag) {
-				/*
-					If all unlocked gotis are at the last part check if move is actually possible according to their current positions and die value.
-					If yes, ask for the move
-					If no, give turn to next player if die < 6.
-					If no & die = 6, Unlock a goti if there are locked gotis; and give a re-chance to roll the die.
-				*/
-				try {
-					const { isPossible } = moveGoti(	// we only need the isPossible array to check if any possible moves are left
-						['G', 'Y', 'B', 'R'][turn],
-						coords,	// starting coordinates to check
-						roll	// distance to move from each coord
-					);
 
-					if (isPossible.reduce((total, current) => total + (current), 0) === 0) {
-						if (locked)												// Return 2 => Move always possible
-							resolve(1);											//
-						else													// Return 1 => Move possible only if die = 6
-							resolve(0);											// (By unlocking locked piece)
-					}															//
-					else {														// Return 0 => Move not possible
-						resolve(2);
-					}
-				} catch (err) {
-					alert(typeof err === "string" ? err : "Move nahi kiya ja sakta... Try again...");
-					console.error(err);
+		positions[turn].forEach(value => {
+			if (typeof (value) === 'string') {
+				const coord = value.split('-');											// If gotis are not at the last(colored) part
+				coords.push([parseInt(coord[0]), parseInt(coord[1])]);					// of the board, any move is possible and hence
+				if (coord[turn % 2] !== '7' || (coord[turn % 2] === '7' && ['0', '14'].includes(coord[(turn % 2) ^ 1]))) {	// set flag=false
+					flag = false;
 				}
 			}
-			else {
-				resolve(2);
+			else if (value === false) {													// If any gotis are locked, set locked=true
+				locked = true;
 			}
 		});
+		if (flag) {
+			/*
+				If all unlocked gotis are at the last part check if move is actually possible according to their current positions and die value.
+				If yes, ask for the move
+				If no, give turn to next player if die < 6.
+				If no & die = 6, Unlock a goti if there are locked gotis; and give a re-chance to roll the die.
+			*/
+			const { isPossible } = moveGoti(	// we only need the isPossible array to check if any possible moves are left
+				['G', 'Y', 'B', 'R'][turn],
+				coords,	// starting coordinates to check
+				roll	// distance to move from each coord
+			);
+
+			if (isPossible.reduce((total, current) => total + (current), 0) === 0) {
+				if (locked)												// Return 2 => Move always possible
+					return 1;											//
+				else													// Return 1 => Move possible only if die = 6
+					return 0;											// (By unlocking locked piece)
+			}															//
+			else {														// Return 0 => Move not possible
+				return 2;
+			}
+		}
+		else {
+			return 2;
+		}
 	}
 	const rollDice = () => {
 		if(!choice)	return;
-		// if (choice) {
 
 		setDie(0);
 		const dice = [...document.querySelectorAll('.die-list')];
 		dice.forEach(die => {
 			toggleClasses(die);
 
-			const roll = rollDice();
-			// const roll = 6
+			const roll = rollDiceOnce();
 			setInfo('');
 			setDie(roll);
 			die.dataset.roll = roll;
-			checkPossible(roll).then(possible => {
-				if (possible) {
-					const newplace = ['6-1', '1-8', '8-13', '13-6'];		// Unlock places for each color(according to turn)
-					// const newplace = [ '7-1', '1-7', '7-13', '13-7' ]
-					let newboard = { ...board };
-					if (roll === 6) {										// If the die rolls a 6:
-						let position = [...positions];
-						if ((position[turn].reduce((total, curr) => total + (curr === false), 0) === position[turn].reduce((total, curr) => total - (curr === true), 4)) || possible === 1 ||
-							((position[turn].reduce((total, curr) => total + (curr === false), 0) > 0) && (window.confirm('Press \'Ok\' to unlock a new goti, or Press \'Cancel\' to move a previous goti')))) {
-							setInfo(turns[turn] + ' unlocked a goti!');		// Checks if there are any unlocked gotis
-							let i = 0;										// If no, unlock a goti
-							while (position[turn][i++] !== false);			// If yes, check if there are any locked gotis
-							position[turn][--i] = newplace[turn];			// If there are, ask whether player wants to unlock a
-							newboard[newplace[turn]].push(turn);				// goti or move unlocked gotis.
-							setBoard(newboard);								// If there arent, just ask player to move a goti
-						}
-						else if (possible === 2) {
-							setChoice(false);
-						}
-						else {
-							setTurn(turn => (turn + 1) % 4);
-						}
-						setPositions(position);
+			const possible = checkPossible(roll);
+			if (possible) {
+				const newplace = ['6-1', '1-8', '8-13', '13-6'];		// Unlock places for each color(according to turn)
+				// const newplace = [ '7-1', '1-7', '7-13', '13-7' ]
+				let newboard = { ...board };
+				if (roll === 6) {										// If the die rolls a 6:
+					let position = [...positions];
+					if ((position[turn].reduce((total, curr) => total + (curr === false), 0) === position[turn].reduce((total, curr) => total - (curr === true), 4)) || possible === 1 ||
+						((position[turn].reduce((total, curr) => total + (curr === false), 0) > 0) && (window.confirm('Press \'Ok\' to unlock a new goti, or Press \'Cancel\' to move a previous goti')))) {
+						setInfo(turns[turn] + ' unlocked a goti!');		// Checks if there are any unlocked gotis
+						let i = 0;										// If no, unlock a goti
+						while (position[turn][i++] !== false);			// If yes, check if there are any locked gotis
+						position[turn][--i] = newplace[turn];			// If there are, ask whether player wants to unlock a
+						newboard[newplace[turn]].push(turn);				// goti or move unlocked gotis.
+						setBoard(newboard);								// If there arent, just ask player to move a goti
+					}
+					else if (possible === 2) {
+						setChoice(false);
 					}
 					else {
-						if (positions[turn].reduce((total, curr) => total + (curr === false), 0) < 4 && possible === 2)
-							setChoice(false);								// If there are any movable gotis and if move is
-						else												// possible, set choice=false, i.e. ask for move
-							setTurn(turn => (turn + 1) % 4);
+						setTurn(turn => (turn + 1) % 4);
 					}
+					setPositions(position);
 				}
-				else if (roll < 6)
-					setTurn(turn => (turn + 1) % 4);
-				});
-			});
+				else {
+					if (positions[turn].reduce((total, curr) => total + (curr === false), 0) < 4 && possible === 2)
+						setChoice(false);								// If there are any movable gotis and if move is
+					else												// possible, set choice=false, i.e. ask for move
+						setTurn(turn => (turn + 1) % 4);
+				}
+			}
+			else if (roll < 6)
+				setTurn(turn => (turn + 1) % 4);
+		});
 	};
 	return (
 		<div className="App">
--- web/src/services/ludo.js
+++ web/src/services/ludo.js
@@ -1,191 +1,236 @@
-	// implementation from underscore
+// implementation from underscore
 function random(min, max) {
-	if (max == null) {
-	  max = min;
-	  min = 0;
-	}
-	return min + Math.floor(Math.random() * (max - min + 1));
+  if (max == null) {
+    max = min;
+    min = 0;
+  }
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 const corners = {
-	oc: { // outer_corners
-		"0,6": "R",
-		"0,8": "D",
-		"6,0": "R",
-		"6,14": "D",
-		"8,0": "U",
-		"8,14": "L",
-		"14,6": "U",
-		"14,8": "L"
-	},
-	it: { // inner_turns
-		"9,6": "L",
-		"6,5": "U",
-		"8,9": "D",
-		"5,8": "R"
-	}
+  oc: {
+    // outer_corners
+    "0,6": "R",
+    "0,8": "D",
+    "6,0": "R",
+    "6,14": "D",
+    "8,0": "U",
+    "8,14": "L",
+    "14,6": "U",
+    "14,8": "L",
+  },
+  it: {
+    // inner_turns
+    "9,6": "L",
+    "6,5": "U",
+    "8,9": "D",
+    "5,8": "R",
+  },
 };
 
-const homeTurns = { // colour: [coord,direction]
-	R: ["14,7", "U"],
-	G: ["7,0", "R"],
-	Y: ["0,7", "D"],
-	B: ["7,14", "L"]
+const homeTurns = {
+  // colour: [coord,direction]
+  R: ["14,7", "U"],
+  G: ["7,0", "R"],
+  Y: ["0,7", "D"],
+  B: ["7,14", "L"],
 };
 
-function isHomeEnd (coord) {
-	if(Array.isArray(coord))	coord = coord.join();
+function isHomeEnd(coord) {
+  if (Array.isArray(coord)) coord = coord.join();
 
-	const home_ends = ['8,7', '6,7', '7,6', '7,8'];
-	if (home_ends.includes(coord)) return true;
-	else return false;
+  const home_ends = ["8,7", "6,7", "7,6", "7,8"];
+  if (home_ends.includes(coord)) return true;
+  else return false;
 }
 
-function turnAtCorner (corners_vec, coord) { // returns `null` to signify to direction
-	return corners_vec[coord] || null;
+function turnAtCorner(corners_vec, coord) {
+  // returns `null` to signify to direction
+  return corners_vec[coord] || null;
 }
 
 function getDirection(coords) {
-	if (!coords || typeof (coords[0]) !== "number" || typeof (coords[0]) !== "number") return null;
+  if (!coords || typeof coords[0] !== "number" || typeof coords[0] !== "number")
+    return null;
 
-	if (coords[1] > 5 && coords[1] < 9) {
-		if (coords[0] === 0) return "R";
-		else if (coords[0] === 14) return "L";
+  if (coords[1] > 5 && coords[1] < 9) {
+    if (coords[0] === 0) return "R";
+    else if (coords[0] === 14) return "L";
 
-		return coords[1] === 6 ? "U" : (coords[1] === 8 ? "D" : coords[0] < 6 ? "D" : "U"); // returning null for home path, since that is retrieved by homeTurns
-	} else if (coords[0] > 5 && coords[0] < 9) {
-		if (coords[1] === 0) return "U";
-		else if (coords[1] === 14) return "D";
+    return coords[1] === 6
+      ? "U"
+      : coords[1] === 8
+      ? "D"
+      : coords[0] < 6
+      ? "D"
+      : "U"; // returning null for home path, since that is retrieved by homeTurns
+  } else if (coords[0] > 5 && coords[0] < 9) {
+    if (coords[1] === 0) return "U";
+    else if (coords[1] === 14) return "D";
 
-		return coords[0] === 6 ? "R" : (coords[0] === 8 ? "L" : coords[1] < 6 ? "R" : "L"); // returning null for home path, since that is retrieved by homeTurns
-	}
+    return coords[0] === 6
+      ? "R"
+      : coords[0] === 8
+      ? "L"
+      : coords[1] < 6
+      ? "R"
+      : "L"; // returning null for home path, since that is retrieved by homeTurns
+  }
 }
 
 function validData(colour, coords, dist) {
-	const allowed_colours = ["R", "G", "Y", "B"];
+  const allowed_colours = ["R", "G", "Y", "B"];
 
-	// also verifies if coords is a 2 dimensional array (expected 2D array) or not
-	if ( !!colour && Array.isArray(coords) && Array.isArray(coords[0]) && !!dist ) {
-		return allowed_colours.includes(colour);
-	} else return false;
+  // also verifies if coords is a 2 dimensional array (expected 2D array) or not
+  if (!!colour && Array.isArray(coords) && Array.isArray(coords[0]) && !!dist) {
+    return allowed_colours.includes(colour);
+  } else return false;
 }
 
 // 6,1, 1, G
 function mover(colour, coord, dist) {
-	let increment_coords = [0, 0];
-	let turnDirection = null;
-	const updated_coords = coord;
-	let currDirection = getDirection(updated_coords);
-	dist = parseInt(dist);	// if it's string, convert to number
+  let increment_coords = [0, 0];
+  let turnDirection = null;
+  const updated_coords = coord;
+  let currDirection = getDirection(updated_coords);
+  dist = parseInt(dist); // if it's string, convert to number
 
-	if (!currDirection || !dist || typeof (colour) !== "string") return [false];
+  if (!currDirection || !dist || typeof colour !== "string") return [false];
 
-	if (dist === 0) return [false];
-	while (dist-- > 0) {
-		increment_coords = [0, 0];
+  if (dist === 0) return [false];
+  while (dist-- > 0) {
+    increment_coords = [0, 0];
 
-		turnDirection = turnAtCorner(corners.oc, updated_coords);
-		if (turnDirection) {
-			currDirection = turnDirection;
-			switch (currDirection) {
-			case "U": increment_coords = [-1, 0]; break;
-			case "L": increment_coords = [0, -1]; break;
-			case "R": increment_coords = [0, 1]; break;
-			case "D": increment_coords = [1, 0]; break;
-			}
-		} else {
-			turnDirection = turnAtCorner(corners.it, updated_coords);
+    turnDirection = turnAtCorner(corners.oc, updated_coords);
+    if (turnDirection) {
+      currDirection = turnDirection;
+      switch (currDirection) {
+        case "U":
+          increment_coords = [-1, 0];
+          break;
+        case "L":
+          increment_coords = [0, -1];
+          break;
+        case "R":
+          increment_coords = [0, 1];
+          break;
+        case "D":
+          increment_coords = [1, 0];
+          break;
+      }
+    } else {
+      turnDirection = turnAtCorner(corners.it, updated_coords);
 
-			if (turnDirection) {
-				currDirection = turnDirection;
-				switch (currDirection) {
-				case "U": increment_coords = [-1, 1]; break;
-				case "L": increment_coords = [-1, -1]; break;
-				case "R": increment_coords = [1, 1]; break;
-				case "D": increment_coords = [1, -1]; break;
-				}
-			} else {
-				if (updated_coords == homeTurns[colour][0]) { currDirection = homeTurns[colour][1]; }
+      if (turnDirection) {
+        currDirection = turnDirection;
+        switch (currDirection) {
+          case "U":
+            increment_coords = [-1, 1];
+            break;
+          case "L":
+            increment_coords = [-1, -1];
+            break;
+          case "R":
+            increment_coords = [1, 1];
+            break;
+          case "D":
+            increment_coords = [1, -1];
+            break;
+        }
+      } else {
+        if (updated_coords == homeTurns[colour][0]) {
+          currDirection = homeTurns[colour][1];
+        }
 
-				switch (currDirection) {
-				case "U": increment_coords = [-1, 0]; break;
-				case "L": increment_coords = [0, -1]; break;
-				case "R": increment_coords = [0, 1]; break;
-				case "D": increment_coords = [1, 0]; break;
-				}
-			}
-		}
+        switch (currDirection) {
+          case "U":
+            increment_coords = [-1, 0];
+            break;
+          case "L":
+            increment_coords = [0, -1];
+            break;
+          case "R":
+            increment_coords = [0, 1];
+            break;
+          case "D":
+            increment_coords = [1, 0];
+            break;
+        }
+      }
+    }
 
-		updated_coords[0] += increment_coords[0];
-		updated_coords[1] += increment_coords[1];
+    updated_coords[0] += increment_coords[0];
+    updated_coords[1] += increment_coords[1];
 
-		if (isHomeEnd(updated_coords) && dist > 0) return [false];
-	}
+    if (isHomeEnd(updated_coords) && dist > 0) return [false];
+  }
 
-	return [
-		true,	// possibility
-		updated_coords	// final coords
-	];
+  return [
+    true, // possibility
+    updated_coords, // final coords
+  ];
 }
 
 export function moveGoti(colour, coords, dist) {
-	if( Array.isArray(coords) && !Array.isArray(coords[0]) ){	// ie. is a 1D array (ie. a pair, array of length 2)
-		coords = [coords];
-	}
+  if (Array.isArray(coords) && !Array.isArray(coords[0])) {
+    // ie. is a 1D array (ie. a pair, array of length 2)
+    coords = [coords];
+  }
 
-	if (!validData(colour, coords, dist)) {
-		// return new Error("Input Not Valid");
-		console.error("Input not valid: ", colour, coords, dist);
-		return [false];
-	}
+  if (!validData(colour, coords, dist)) {
+    // return new Error("Input Not Valid");
+    console.error("Input not valid: ", colour, coords, dist);
+    return [false];
+  }
 
-	const bools = [];	// an array of bools
-	const finalCoords = [];	// array of final coordinates
+  const bools = []; // an array of bools
+  const finalCoords = []; // array of final coordinates
 
-	let possibility, finalCoord;
-	coords.forEach(coord => {
-		if(!Array.isArray(coords)){ console.error(coords, "was the input array, not a array of pairs !");	return new Error("Invalid Move");	}
+  let possibility, finalCoord;
+  coords.forEach((coord) => {
+    [possibility, finalCoord] = mover(colour, coord, dist);
+    bools.push(possibility);
+    finalCoords.push(finalCoord);
+  });
 
-		[possibility, finalCoord] = mover(colour, coord, dist);
-		bools.push(possibility);
-		finalCoords.push(finalCoord);
-	});
+  console.debug(`Input: ${coords}, moved ${dist} units, final result: `, {
+    isPossible: bools,
+    finalCoords,
+  });
 
-	console.debug(`Input: ${coords}, moved ${dist} units, final result: `, {isPossible: bools, finalCoords});
-
-	return {
-		isPossible: bools,
-		finalCoords: finalCoords
-	};
+  return {
+    isPossible: bools,
+    finalCoords: finalCoords,
+  };
 }
 
-export function rollDice() {
-	return random(1,6);
+export function rollDiceOnce() {
+  return random(1, 6);
 }
 
 export function multipleRoll() {
-	let n = random(1, 6);
-	const arr = [];
-	arr.push(n);
+  let n = random(1, 6);
+  const arr = [];
+  arr.push(n);
 
-	let six_count = 0;
-	while (n === 6) {
-		n = random(1, 6);
+  let six_count = 0;
+  while (n === 6) {
+    n = random(1, 6);
 
-		six_count++;	// to remove three consecutive sixes
-		if(six_count === 3) {
-			arr.pop();
-			arr.pop();	// previous two were 6, and this turn 6 won't be pushed so only two 6 in array
+    six_count++; // to remove three consecutive sixes
+    if (six_count === 3) {
+      arr.pop();
+      arr.pop(); // previous two were 6, and this turn 6 won't be pushed so only two 6 in array
 
-			six_count = 0;
-			continue;
-		}
+      six_count = 0;
+      continue;
+    }
 
-		arr.push(n);
-	}
+    arr.push(n);
+  }
 
-	return arr;
+  return arr;
 }
 
-moveGoti('G', [6,1], 1);
-moveGoti('R', [13,6], 5);
+moveGoti("G", [6, 1], 1);
+moveGoti("R", [13, 6], 5);
